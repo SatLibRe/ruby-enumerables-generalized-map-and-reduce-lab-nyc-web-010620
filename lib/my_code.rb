9866ac7234fc8173d053c525b0858b84892c09fb
Miles Higbie <miles.higbie@gmail.com>
2019-12-17 21:25:20 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,24 +1,25 @@
-def map(array)
-  new_arr = []
-  i = 0
-    while i < array.length do 
-      new_arr.push(yield(array[i]))
-        i += 1 
-    end 
-  return new_arr
-end 
+# def map(array)
+#   new_arr = []
+#   i = 0
+#     while i < array.length do 
+#       new_arr.push(yield(array[i]))
+#         i += 1 
+#     end 
+#   return new_arr
+# end 
 
-def reduce(array, sv = nil)
-  if sv 
-    sum = sv 
-    i = 0 
-  else 
-    sum = array[0]
-    i = 1
-  end
-    while i < array.length do 
-      sum  = yield(sum, array[i])
-    i += 1
-    end 
-  return sum
-end 
+# def reduce(array, sv = nil)
+#   if sv 
+#     sum = sv 
+#     i = 0 
+#   else 
+#     sum = array[0]
+#     i = 1
+#   end
+#     while i < array.length do 
+#       sum  = yield(sum, array[i])
+#     i += 1
+#     end 
+#   return sum
+# end 
+
